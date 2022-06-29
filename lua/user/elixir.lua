5e9eb9705928e82f7027e49b6c77674c3c7e937f
Adriano Mota <adrianowsh@hotmail.com>
2022-06-29 20:09:40 -0300

    change colorscheme

--- lua/user/elixir.lua
+++ lua/user/elixir.lua
@@ -26,11 +26,11 @@ elixir.setup({
     vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", map_opts)
 
     -- standard lsp keybinds
-    vim.keymap.set("n", "df", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", map_opts)
+    vim.keymap.set("n", "<A-f>", "<cmd>lua vim.lsp.buf.formatting_seq_sync()<cr>", map_opts)
     vim.keymap.set("n", "gd", "<cmd>lua vim.diagnostic.open_float()<cr>", map_opts)
-    vim.keymap.set("n", "dt", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
+    vim.keymap.set("n", "<A-d>", "<cmd>lua vim.lsp.buf.definition()<cr>", map_opts)
     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", map_opts)
-    vim.keymap.set("n", "gD","<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
+    vim.keymap.set("n", "<A-i>","<cmd>lua vim.lsp.buf.implementation()<cr>", map_opts)
     vim.keymap.set("n", "1gD","<cmd>lua vim.lsp.buf.type_definition()<cr>", map_opts)
     -- keybinds for fzf-lsp.nvim: https://github.com/gfanto/fzf-lsp.nvim
     -- you could also use telescope.nvim: https://github.com/nvim-telescope/telescope.nvim
